159a27a7756f08644bc458dc1b1826d42b60d5af
bgowaski <gowaski.b@gmail.com>
2016-02-10 17:17:51 -0500

    project proposal

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/ben_homepage'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.get('/hello', function(req, res){
     res.send('hello world');
 });
+
+app.get('/', function(req, res){
+    res.sendfile((__dirname + '/index.html'));
+});
+
 app.listen(port, ipaddress);
